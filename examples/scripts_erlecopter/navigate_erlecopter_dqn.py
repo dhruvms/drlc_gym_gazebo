3ccf11aee5e39a45f0efa08fcf965a7a1cee452f
madratman <ratneshmadaan@gmail.com>
2017-04-13 21:01:43 -0400

    adds PWM actions lol

--- examples/scripts_erlecopter/navigate_erlecopter_dqn.py
+++ examples/scripts_erlecopter/navigate_erlecopter_dqn.py
@@ -17,42 +17,6 @@ from deeprl_hw2.objectives import *
 import gym
 import gym_gazebo
 
-def get_output_folder(parent_dir, env_name):
-    """Return save folder.
-
-    Assumes folders in the parent_dir have suffix -run{run
-    number}. Finds the highest run number and sets the output folder
-    to that number + 1. This is just convenient so that if you run the
-    same script multiple times tensorboard can plot all of the results
-    on the same plots with different names.
-
-    Parameters
-    ----------
-    parent_dir: str
-      Path of the directory containing all experiment runs.
-
-    Returns
-    -------
-    parent_dir/run_dir
-      Path to this run's save directory.
-    """
-    os.makedirs(parent_dir)
-    experiment_id = 0
-    for folder_name in os.listdir(parent_dir):
-        if not os.path.isdir(os.path.join(parent_dir, folder_name)):
-            continue
-        try:
-            folder_name = int(folder_name.split('-run')[-1])
-            if folder_name > experiment_id:
-                experiment_id = folder_name
-        except:
-            pass
-    experiment_id += 1
-
-    parent_dir = os.path.join(parent_dir, env_name)
-    parent_dir = parent_dir + '-run{}'.format(experiment_id)
-    return parent_dir
-
 def main():  # noqa: D103
     parser = argparse.ArgumentParser(description='Run DQN on Atari SpaceInvaders')
     parser.add_argument('--env', default='GazeboErleCopterNavigate-v0', help='Atari env name')
--- gym_gazebo/envs/navigate_erlecopter_env.py
+++ gym_gazebo/envs/navigate_erlecopter_env.py
@@ -21,7 +21,7 @@ from sensor_msgs.msg import LaserScan, NavSatFix, Image
 from std_msgs.msg import Float64
 from gazebo_msgs.msg import ModelStates, ContactState
 from geometry_msgs.msg import TwistStamped
-
+from geometry_msgs.msg import PoseStamped
 import tf
 
 class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
@@ -179,18 +179,8 @@ class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
 		# Launch the simulation with the given launchfile name
 		gazebo_env.GazeboEnv.__init__(self, "GazeboErleCopterHover-v0.launch")    
 
-		self.action_space = spaces.Discrete(10) # F, L, R, B
-		#self.observation_space = spaces.Box(low=0, high=20) #laser values
+		self.action_space = spaces.Discrete(7) # F, L, R, B
 		self.reward_range = (-np.inf, np.inf)
-
-		self.initial_latitude = None
-		self.initial_longitude = None
-
-		self.current_latitude = None
-		self.current_longitude = None
-
-		self.diff_latitude = None
-		self.diff_longitude = None
 
 		self.max_distance = 1.6
 		self.vel_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
@@ -204,6 +194,7 @@ class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
 		self.arm_proxy = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
 		self.takeoff_proxy = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
 		self.pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=1)
+		self.pose_subscriber = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.pose_callback)
 
 		self.rtl_time = 5
 		self.reset_time = 3
@@ -243,40 +234,71 @@ class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
 	def _state(self, action):
 		return discretized_ranges, done
 
+	def pose_callback(self, msg):
+		position = msg.pose.position
+		quat = msg.pose.orientation
+		# rospy.loginfo("Point Position: [ %f, %f, %f ]"%(position.x, position.y, position.z))
+		# rospy.loginfo("Quat Orientation: [ %f, %f, %f, %f]"%(quat.x, quat.y, quat.z, quat.w))
+		euler = tf.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+		# rospy.loginfo("Euler Angles: %s"%str(euler))
+
 	def _step(self, action):
-		vel_cmd = TwistStamped()
-		now = rospy.get_rostime()
-		vel_cmd.header.stamp.secs = now.secs
-		vel_cmd.header.stamp.nsecs = now.nsecs
 
-		speed = 1
-		pi = math.pi
+		print "Taking action", action
+		self.msg = OverrideRCIn()
+		mean_yaw_pwm = 1500
+		delta = 50
+		if action == 0: #FORWARD
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm  # Yaw
+		elif action == 1: 
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm + delta # Yaw
+		elif action == 2: 
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm + delta*1  # Yaw
+		elif action == 3: 
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm + delta*2 #Yaw
+		elif action == 4:
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm - delta #Yaw
+		elif action == 5:
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm - delta*2 #Yaw
+		elif action == 6:
+			self.msg.channels[1] = 1450 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm - delta*3 #Yaw
+		elif action == 7:
+			self.msg.channels[1] = 1550 # Pitch
+			self.msg.channels[3] = mean_yaw_pwm #Yaw
 
-		vel_cmd.twist.linear.x = speed*math.cos(action*(pi/10))
-		vel_cmd.twist.linear.y = speed*math.sin(action*(pi/10))
-		vel_cmd.twist.linear.z = 0
-		# quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-		print "taking action", action, ":: velocity (x,y,z)", vel_cmd.twist.linear.x, vel_cmd.twist.linear.y, vel_cmd.twist.linear.z
-		self.vel_pub.publish(vel_cmd)
+		self.msg.channels[0] = 0 # Roll
+		self.msg.channels[2] = 1500 # Throttle
+		self.msg.channels[4] = 0
+		self.msg.channels[5] = 0
+		self.msg.channels[6] = 0
+		self.msg.channels[7] = 0
+
+		self.pub.publish(self.msg)
+
+		# vel_cmd = TwistStamped()
+		# now = rospy.get_rostime()
+		# vel_cmd.header.stamp.secs = now.secs
+		# vel_cmd.header.stamp.nsecs = now.nsecs
+
+		# speed = 5
+		# pi = math.pi
+
+		# vel_cmd.twist.linear.x = speed*math.cos(action*(pi/10))
+		# vel_cmd.twist.linear.y = speed*math.sin(action*(pi/10))
+		# vel_cmd.twist.linear.z = 0
+		# # quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+		# print "taking action", action, ":: velocity (x,y,z)", vel_cmd.twist.linear.x, vel_cmd.twist.linear.y, vel_cmd.twist.linear.z
+		# self.vel_pub.publish(vel_cmd)
 	
 		observation = self._get_frame()
 		
-
-		# data = None
-		# while data is None:
-		# 	try:
-		# 		data = rospy.wait_for_message('gazebo/circle/physics/contact', ContactState, timeout = 5)
-		# 	except:
-		# 		pass
-
-		# if len(data.contact_positions) > 0:
-		# 	reward = -100
-		# 	is_terminal = True
-		# else:
-		# 	reward = 10
-		# 	is_terminal = False
-
-
 		data = None
 		while data is None:
 			try:
@@ -285,12 +307,17 @@ class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
 				pass
 
 		# is_terminal = self.check_terminal(data)
-		state,is_terminal = self.discretize_observation(data,len(data.ranges))
+		print "min laser", np.min(data.ranges)
+		# print "max laser", np.max(data.ranges)
+		state, is_terminal = self.discretize_observation(data,len(data.ranges))
 
-		if is_terminal:
-			reward = -100
+		if not is_terminal:
+			if action == 0:
+				reward = 5
+			else:
+				reward = 1
 		else:
-			reward = 10 
+			reward = -200
 
 		return observation, reward, is_terminal, {}	
 
@@ -301,17 +328,25 @@ class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
 				frame = rospy.wait_for_message('/camera/rgb/image_raw',Image, timeout = 5)
 				cv_image = CvBridge().imgmsg_to_cv2(frame, desired_encoding="passthrough")
 				frame = np.asarray(cv_image)
+				cv2.imshow('frame', frame)
+				cv2.waitKey(10)
 				return frame
 			except:
 				raise ValueError('could not get frame')
 
 	def _reset(self):
-
+		self.msg.channels[0] = 0
+		self.msg.channels[1] = 0
 		self.msg.channels[2] = 0
+		self.msg.channels[3] = 0
+		self.msg.channels[4] = 0
+		self.msg.channels[5] = 0
+		self.msg.channels[6] = 0
+		self.msg.channels[7] = 0
 		rospy.loginfo('Sending RC THROTTLE %d', self.msg.channels[2])
 		self.pub.publish(self.msg)
 
-		time.sleep(1)
+		time.sleep(5)
 
 		rospy.loginfo('Changing mode to RTL')
 		# Set RTL mode
@@ -330,22 +365,15 @@ class GazeboErleCopterNavigateEnv(gazebo_env.GazeboEnv):
 
 		self._takeoff(2)
 		
+		self.initial_latitude = None
+		self.initial_longitude = None
+
 		return self._get_frame()
-
-	# def _update_reward(self, data):
-	#     if len(data.contact_positions) >0:
-	#          self.b_collision = True
-
-	def check_terminal(delf, data):
-		if data.range_min < 0.01:
-			return False
-		else:
-			return True 
 
 	def discretize_observation(self,data,new_ranges):
 		# print data
 		discretized_ranges = []
-		min_range = 0.5
+		min_range = 2.5
 		done = False
 		mod = len(data.ranges)/new_ranges
 		for i, item in enumerate(data.ranges):
