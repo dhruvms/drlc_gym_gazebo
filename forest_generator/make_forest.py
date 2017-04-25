33d0b67591d1f96e71378447a8f29d7aa5c94331
madratman <ratneshmadaan@gmail.com>
2017-04-25 02:09:32 -0400

    adds @rogeriobonatti 's world generator with mods

---
+++ forest_generator/make_forest.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+# setup file to be written
+filename='/home/vaibhav/madratman/projects/inspection_sim_ws/src/inspection_sim/dji_gazebo/worlds/final_forest_final.world'
+target=open(filename,'w')
+
+# read beginning of file
+string = open('begin3_madratman', 'r').read()
+cil_1 = open('cil_13_madratman', 'r').read()
+cil_2 = open('cil_23_madratman', 'r').read()
+
+# write beginning of file
+target.write(string)
+
+# generate random samples
+
+nx=10
+ny=5
+spacing=5
+random_interval_x=5
+random_interval_y=2.5
+offset_x=5
+offset_y=-int(ny*spacing/2)
+
+x = np.linspace(offset_x, offset_x+(nx-1)*spacing, nx)
+y = np.linspace(offset_y, offset_y+(ny-1)*spacing, ny)
+
+positions_x=np.zeros([nx,ny])
+positions_y=np.zeros([nx,ny])
+
+counter=0
+
+for i in range(nx):
+	for j in range(ny):
+		name="\n    <model name='unit_cylinder_"+str(counter)+"'>"
+		counter+=1
+		target.write(name)
+		noise_x=np.random.random()-0.5
+		noise_x*=random_interval_x
+		noise_y=np.random.random()-0.5
+		noise_y*=random_interval_y
+		x_tree=x[i]+noise_x
+		y_tree=y[j]+noise_y
+		positions_x[i,j]=x_tree
+		positions_y[i,j]=y_tree
+		line_to_print="\n      <pose frame=''>"+str(x_tree)+" "+str(y_tree)+" 3 0 -0 0</pose>\n"
+		target.write(line_to_print)
+		target.write(cil_1)
+
+target.write("\n")
+target.write( "</world>\n</sdf>\n")
+target.close()
+
+np.savetxt('pos_x.out', positions_x, delimiter=',')
+np.savetxt('pos_y.out', positions_y, delimiter=',')
+
