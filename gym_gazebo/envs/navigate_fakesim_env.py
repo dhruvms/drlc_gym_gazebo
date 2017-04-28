a5e757b70d7d0ef87fa1b2b5f87503af532ea8ec
madratman <ratneshmadaan@gmail.com>
2017-04-28 02:41:15 -0400

    fix ghost mode bug by publishing zero vel after time.sleep() in step(), reset dji before reset cylinder, pretty printing, add reset argument to run_forest.sh

--- gym_gazebo/envs/navigate_fakesim_env.py
+++ gym_gazebo/envs/navigate_fakesim_env.py
@@ -63,16 +63,18 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 
 		self.laser_subscriber = message_filters.Subscriber('/scan', LaserScan)
 		self.image_subscriber = message_filters.Subscriber('/camera/rgb/image_raw', Image)
-		self.synchro = message_filters.ApproximateTimeSynchronizer([self.laser_subscriber, self.image_subscriber], 1, 0.05)
+		self.synchro = message_filters.ApproximateTimeSynchronizer([self.laser_subscriber, self.image_subscriber], 10, 0.5)
 		self.synchro.registerCallback(self.synchro_callback)
 
 		self.observation = None
 		self.laser = None
 		self.HAVE_DATA = False
+		self.first = True
 
 		# self.get_model_state_proxy = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 		self.set_model_state_proxy = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
+	# checks for bot's pose and sets self.is_terminal to True if it goes outside the forest'
 	def pose_callback(self, msg):
 		self.position =  msg.pose.pose.position
 
@@ -82,6 +84,11 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 			print "went out of range. ending episode"
 			rospy.loginfo("Point Position: [ %f, %f, %f ]"%(self.position.x, self.position.y, self.position.z))
 
+			# experimental : set dji pose here itself. this can cause a bad callback error sometimes. tocheck
+			# self.reset_dji()
+
+	# where there is an image, let there be a laser message.
+	# into that heaven of learning, let my bot awake  
 	def synchro_callback(self, laser, image):
 		cv_image = CvBridge().imgmsg_to_cv2(image, desired_encoding="passthrough")
 		self.observation = np.asarray(cv_image)
@@ -95,13 +102,15 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 
 	def _step(self, action):
 		vel_cmd = Twist()
-		speed = 2.5
+		speed = 5.0
+		delta_theta_deg = 10 # diff of heading (or pseudo-heading) between each action
 
-		delta_theta_deg = 10
+		#### action set with varying heading ####
 		# 4 is forward, 0-3 are to left, 5-8 are right. all separated by 10 deg each.
 		action_norm = action - ((self.num_actions-1)/2)
 		# 0 is forward in action_norm. negatives are left
 
+		#### action set with same heading ####
 		# vel_x_body = speed*math.cos(action_norm*(math.radians(delta_theta_deg)))
 		# vel_y_body = speed*math.sin(action_norm*(math.radians(delta_theta_deg)))
 		# vel_cmd.linear.x = vel_x_body
@@ -111,11 +120,18 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 		vel_cmd.linear.x = speed
 		vel_cmd.angular.z = action_norm*(math.radians(delta_theta_deg))
 		self.vel_pub.publish(vel_cmd)
-
+		time.sleep(0.1)
+		# pubish zero after sleeping for a small time to avoid ghost mode bug
+		vel_cmd_zero = Twist()
+		self.vel_pub.publish(vel_cmd_zero)
+		
+		# keep on waiting for getting laser data
 		self.HAVE_DATA = False
 		while not self.HAVE_DATA:
+			# print "step() : self.HAVE_DATA is False!"
 			continue
 
+		# find distance to goal and give some reward based on it 
 		dist_to_goal = math.sqrt((self.position.y - 0.0)**2 + (self.position.x - self.MAX_POSITION_X)**2)
 		# reward_dist_to_goal = 1 / dist_to_goal
 		reward_dist_to_goal = (self.MAX_POSITION_X-dist_to_goal) / float(self.MAX_POSITION_X)
@@ -139,31 +155,56 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 		else:
 			reward = self.REWARD_AT_CRASH
 
-		if action_norm < 0:
-			print "min_laser : {} dist_to_goal : {} reward_dist_to_goal : {} action : {} reward : {}".format(round(self.min_laser_scan,2), round(dist_to_goal,2), \
-						round(reward_dist_to_goal,2), action_norm, round(reward,2))
-			# print "min_laser : {} action : {} reward : {}".format(round(self.min_laser_scan,2), action_norm, reward)
-		else:
-			print "min_laser : {} dist_to_goal : {} reward_dist_to_goal : {} action : +{} reward : {}".format(round(self.min_laser_scan,2), round(dist_to_goal,2), \
-						round(reward_dist_to_goal,2), action_norm, round(reward,2))
-			# print "min_laser : {} action : +{} reward : {}".format(round(self.min_laser_scan,2), action_norm, reward)
+		print "min_laser : {:.2f} dist_to_goal : {:.2f} reward_dist_to_goal : {:.2f} action : {:+d} reward : {:+.2f}"\
+			.format(self.min_laser_scan, dist_to_goal, reward_dist_to_goal, action_norm, reward)
 
 		return self.observation, reward, self.done, {}	
 
-	def _reset(self):
-		vel_cmd = Twist() # zero msg
-		self.vel_pub.publish(vel_cmd)
-		# time.sleep(1)
-		rospy.loginfo('Gazebo RESET')
-		fuck_ctr = 0
-		# subprocess.Popen(["python","/home/vaibhav/madratman/drlc_gym_gazebo/forest_generator/make_forest.py"])
+	# utility function to set the bot's pose
+	def reset_dji(self):
+		# until the dji pose is actually set. This condition is added as there's some weird thing in the sim which causes the bot's' urdf
+		# to fluctuate between it's last position and the reset position.  
+		while not (self.reset_position.x == self.position.x) and \
+			  not (self.reset_position.y == self.position.y) and \
+			  not (self.reset_position.z == abs(self.position.z)):
 		
-		# EPSILON = 1e-100
-		# while (not abs(self.reset_position.x-self.position.x) < EPSILON) and \
-			  # (not abs(self.reset_position.y-self.position.y) < EPSILON) and \
-			  # (not abs(self.reset_position.z-self.position.z) < EPSILON):
+			model_pose = Pose()
+			model_pose.position.x = self.reset_position.x
+			model_pose.position.y = self.reset_position.y
+			model_pose.position.z = self.reset_position.z
+			model_pose.orientation.x = 0.0
+			model_pose.orientation.y = 0.0
+			model_pose.orientation.z = 0.0
+			model_pose.orientation.w = 1.0
 
-			# generate random samples
+			model_twist = Twist()
+
+			model_state = ModelState()
+			model_state.model_name = 'dji'
+			model_state.pose = model_pose
+			model_state.twist = model_twist
+			model_state.reference_frame = 'world' # change to 'world'?
+			rospy.wait_for_service('/gazebo/set_model_state')
+			try:
+				self.set_model_state_proxy(model_state)
+				# rospy.loginfo("DJI position updated. Point Position: [ %f, %f, %f ]"%(self.position.x, self.position.y, self.position.z))
+				# print(self.reset_position.x, self.reset_position.y, self.reset_position.z)
+				# print(self.reset_position.x == self.position.x, self.reset_position.y == self.position.y, self.reset_position.z == self.position.z)
+			except rospy.ServiceException, e:
+				print "Service call failed: %s"%e
+
+		rospy.loginfo("DJI position updated")
+
+		# recursive to ensure
+		if not (self.reset_position.x == self.position.x) and \
+			  not (self.reset_position.y == self.position.y) and \
+			  not (self.reset_position.z == abs(self.position.z)):
+			  print "reset_dji() : recursion "
+			  self.reset_dji()
+		
+	# generate random poses for trees and call set model pose for each tree 
+	def make_a_brave_new_forest(self):
+		# generate random samples
 		nx = 15
 		spacing_x = 6
 		random_interval_x = spacing_x/3
@@ -176,9 +217,6 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 
 		x = np.linspace(offset_x, offset_x+(nx-1)*spacing_x, nx)
 		y = np.linspace(offset_y, offset_y+(ny-1)*spacing_y, ny)
-
-		# positions_x=np.zeros([nx,ny])
-		# positions_y=np.zeros([nx,ny])
 
 		counter=0
 		np.random.seed() #use seed from sys time to build new env on reset
@@ -209,7 +247,7 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 				model_state.model_name = name
 				model_state.pose = model_pose
 				model_state.twist = model_twist
-				model_state.reference_frame = '' # change to 'world'?
+				model_state.reference_frame = 'world' # change to 'world'?
 				rospy.wait_for_service('/gazebo/set_model_state')
 				try:
 					self.set_model_state_proxy(model_state)
@@ -218,38 +256,26 @@ class GazeboErleCopterNavigateEnvFakeSim(gym.Env):
 
 		rospy.loginfo("Cylinder positions updated.")
 
-		while not (self.reset_position.x == self.position.x) and \
-			  not (self.reset_position.y == self.position.y) and \
-			  not (self.reset_position.z == abs(self.position.z)):
-		
-			model_pose = Pose()
-			model_pose.position.x = self.reset_position.x
-			model_pose.position.y = self.reset_position.y
-			model_pose.position.z = self.reset_position.z
-			model_pose.orientation.x = 0.0
-			model_pose.orientation.y = 0.0
-			model_pose.orientation.z = 0.0
-			model_pose.orientation.w = 1.0
+	def _reset(self):
+		if not self.first:
+			vel_cmd = Twist() # zero msg
+			self.vel_pub.publish(vel_cmd)
+			rospy.loginfo('reset called()')
 
-			model_twist = Twist()
+			# reset drone
+			self.reset_dji()
 
-			model_state = ModelState()
-			model_state.model_name = 'dji'
-			model_state.pose = model_pose
-			model_state.twist = model_twist
-			model_state.reference_frame = 'world' # change to 'world'?
-			rospy.wait_for_service('/gazebo/set_model_state')
-			try:
-				self.set_model_state_proxy(model_state)
-				rospy.loginfo("DJI position updated. Point Position: [ %f, %f, %f ]"%(self.position.x, self.position.y, self.position.z))
-				print(self.reset_position.x, self.reset_position.y, self.reset_position.z)
-				print(self.reset_position.x == self.position.x, self.reset_position.y == self.position.y, self.reset_position.z == self.position.z)
-			except rospy.ServiceException, e:
-				print "Service call failed: %s"%e
+			# make a new forest
+			self.make_a_brave_new_forest()
 
-		self.HAVE_DATA = False
-		while not self.HAVE_DATA:
-			continue
+			self.HAVE_DATA = False
 
-		self.done = False
+			while not self.HAVE_DATA:
+				# print "_reset() :: self.HAVE_DATA is False!"
+			
+				continue
+			self.done = False
+
+		self.first = False
+
 		return self.observation
